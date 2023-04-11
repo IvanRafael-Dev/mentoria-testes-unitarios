7421e8b0eebc7ff8cfcb98a850e1a03d2cf3822a
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-04-11 16:27:41 -0300

    test: ensure SignUpController.create calls next with correct error if validation throws

--- src/presentation/controllers/signup/SignUp.spec.ts
+++ src/presentation/controllers/signup/SignUp.spec.ts
@@ -1,30 +1,28 @@
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 import sinon from 'sinon';
 import SignUpController from './SignUp';
 import MakeSinonReqRes, { ReqResNext } from '../../../test/utils/makeReqRes';
 import { Validation } from '../../../services/validation/protocols/validation';
+import { User } from '../../../domain/models/User';
 
-interface User {
-  id?: number
-  username: string
-  email: string
-  password: string
-}
+const makeRequestObject = (body: any, params: string): ReqResNext => {
+  return new MakeSinonReqRes(body, params).makeReqRes();
+};
+
+const makeValidateUserFieldsStub = (): Validation => {
+  class ValidateUserFieldsStub implements Validation {
+    validate (user: User): void {}
+  }
+  return new ValidateUserFieldsStub();
+};
 
 interface SutTypes {
   sut: SignUpController
   validateUserFieldsStub: Validation
 }
 
-const makeRequestObject = (body: any, params: string): ReqResNext => {
-  return new MakeSinonReqRes(body, params).makeReqRes();
-};
-
 const makeSut = (): SutTypes => {
-  class ValidateUserFieldsStub implements Validation {
-    validate (user: User): void {}
-  }
-  const validateUserFieldsStub = new ValidateUserFieldsStub();
+  const validateUserFieldsStub = makeValidateUserFieldsStub();
   const sut = new SignUpController(validateUserFieldsStub);
 
   return {
@@ -34,6 +32,19 @@ const makeSut = (): SutTypes => {
 };
 
 describe('SignUpController', () => {
+  it('should call next if validation throws', async () => {
+    const { sut, validateUserFieldsStub } = makeSut();
+    const error = new Error();
+    sinon.mock(validateUserFieldsStub).expects('validate').throws(error);
+    const { req, res, next } = makeRequestObject({
+      username: 'any_username',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }, '');
+    sut.create(req, res, next);
+    expect((next as sinon.SinonSpy).calledWithExactly(error)).to.be.true;
+  });
+
   it('should call Validations with correct values', async () => {
     const { sut, validateUserFieldsStub } = makeSut();
     const validateSpy = sinon.spy(validateUserFieldsStub, 'validate');
