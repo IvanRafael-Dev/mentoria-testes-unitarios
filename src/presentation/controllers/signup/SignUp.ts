3c6b5e27eab2a530aefda851b0755d17f1fe5095
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-04-11 16:10:16 -0300

    feat: ensure SignUpController.create call validation with correct values

--- src/presentation/controllers/signup/SignUp.ts
+++ src/presentation/controllers/signup/SignUp.ts
@@ -1,18 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import MissingParamError from '../../../errors/missing-param-error';
+import { Validation } from '../../../services/validation/protocols/validation';
 
 export default class SignUpController {
-  async create (req: Request, res: Response, _next: NextFunction): Promise<Response | void> {
-    if (!req.body.username) {
-      throw new MissingParamError('Missing param: username');
-    }
+  private readonly _validateUserFields: Validation;
+  constructor (validateUserFields: Validation) {
+    this._validateUserFields = validateUserFields;
+  }
 
-    if (!req.body.email) {
-      throw new MissingParamError('Missing param: email');
-    }
-
-    if (!req.body.password) {
-      throw new MissingParamError('Missing param: password');
-    }
+  create (req: Request, res: Response, next: NextFunction): void {
+    this._validateUserFields.validate(req.body);
   }
 }
