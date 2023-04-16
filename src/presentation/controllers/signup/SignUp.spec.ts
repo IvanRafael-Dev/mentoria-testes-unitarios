65867ea8f60f15ff8bbede09d87fbf7eea96e340
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-04-15 22:52:49 -0300

    feat: ensure SignUpController.create calls Validatons with correct data

--- src/presentation/controllers/signup/SignUp.spec.ts
+++ src/presentation/controllers/signup/SignUp.spec.ts
@@ -4,6 +4,8 @@ import SignUpController from './SignUp';
 import MakeSinonReqRes, { ReqResNext } from '../../../test/utils/makeReqRes';
 import { Validation } from '../../../services/validation/protocols/validation';
 import { User } from '../../../domain/models/User';
+import { AddAccount } from '../../../domain/useCases/addAccount';
+import { AccountModel } from '../../../domain/models/Account';
 
 const makeRequestObject = (body: any, params: string): ReqResNext => {
   return new MakeSinonReqRes(body, params).makeReqRes();
@@ -19,19 +21,39 @@ const makeValidateUserFieldsStub = (): Validation => {
 interface SutTypes {
   sut: SignUpController
   validateUserFieldsStub: Validation
+  addAccountStub: AddAccount
 }
 
 const makeSut = (): SutTypes => {
+  class AddAccountStub implements AddAccount {
+    async register (user: User): Promise<AccountModel> {
+      return { id: 1, ...user };
+    }
+  }
+  const addAccountStub = new AddAccountStub();
   const validateUserFieldsStub = makeValidateUserFieldsStub();
-  const sut = new SignUpController(validateUserFieldsStub);
+  const sut = new SignUpController(validateUserFieldsStub, addAccountStub);
 
   return {
     sut,
-    validateUserFieldsStub
+    validateUserFieldsStub,
+    addAccountStub
   };
 };
 
 describe('SignUpController', () => {
+  it('should call Validations with correct values', async () => {
+    const { sut, validateUserFieldsStub } = makeSut();
+    const validateSpy = sinon.spy(validateUserFieldsStub, 'validate');
+    const { req, res, next } = makeRequestObject({
+      username: 'any_username',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }, '');
+    await sut.create(req, res, next);
+    expect(validateSpy.calledWith(req.body)).to.be.true;
+  });
+
   it('should call next if validation throws', async () => {
     const { sut, validateUserFieldsStub } = makeSut();
     const error = new Error();
@@ -41,19 +63,19 @@ describe('SignUpController', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     }, '');
-    sut.create(req, res, next);
+    await sut.create(req, res, next);
     expect((next as sinon.SinonSpy).calledWithExactly(error)).to.be.true;
   });
 
-  it('should call Validations with correct values', async () => {
-    const { sut, validateUserFieldsStub } = makeSut();
-    const validateSpy = sinon.spy(validateUserFieldsStub, 'validate');
+  it('should call AddAccount with correct values', async () => {
+    const { sut, addAccountStub } = makeSut();
+    const addAccountSpy = sinon.spy(addAccountStub, 'register');
     const { req, res, next } = makeRequestObject({
       username: 'any_username',
       email: 'any_email@mail.com',
       password: 'any_password'
     }, '');
-    sut.create(req, res, next);
-    expect(validateSpy.calledWith(req.body)).to.be.true;
+    await sut.create(req, res, next);
+    expect(addAccountSpy.calledWith(req.body)).to.be.true;
   });
 });
--- src/presentation/controllers/signup/SignUp.ts
+++ src/presentation/controllers/signup/SignUp.ts
@@ -1,15 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { Validation } from '../../../services/validation/protocols/validation';
+import { AddAccount } from '../../../domain/useCases/addAccount';
 
 export default class SignUpController {
   private readonly _validateUserFields: Validation;
-  constructor (validateUserFields: Validation) {
+  private readonly _addAccount: AddAccount;
+  constructor (validateUserFields: Validation, addAccount: AddAccount) {
     this._validateUserFields = validateUserFields;
+    this._addAccount = addAccount;
   }
 
-  create (req: Request, res: Response, next: NextFunction): void {
+  async create (req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       this._validateUserFields.validate(req.body);
+      await this._addAccount.register(req.body);
     } catch (error) {
       next(error);
     }
